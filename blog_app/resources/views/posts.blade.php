f637312dc29cb939f5db1951710d7530f915362a
Garin Mccullick <glmccullick@gmail.com>
2022-11-13 20:47:58 -0600

    rendering html pages

---
+++ blog_app/resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <title>My Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My first post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non neque placeat explicabo excepturi qui in, repudiandae adipisci nobis iure veritatis iste consequatur ratione recusandae tenetur culpa ad rem! Rem!</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-second-post">My second post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non neque placeat explicabo excepturi qui in, repudiandae adipisci nobis iure veritatis iste consequatur ratione recusandae tenetur culpa ad rem! Rem!</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-third-post">My third post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non neque placeat explicabo excepturi qui in, repudiandae adipisci nobis iure veritatis iste consequatur ratione recusandae tenetur culpa ad rem! Rem!</p>
+    </article>
+</body>
+
+</html>
